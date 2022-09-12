b0ab5f8a4200325defb3c6c91bcc6e0022daa680
HolyDemon5 <73434527+HolyDemon5@users.noreply.github.com>
2022-09-12 18:12:01 +0530

    compass

---
+++ Compass/CompassApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompassApp.swift
+//  Compass
+//
+//  Created by Licious on 02/08/21.
+//
+
+import SwiftUI
+
+@main
+struct CompassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Compass/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Compass
+//
+//  Created by Licious on 02/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
